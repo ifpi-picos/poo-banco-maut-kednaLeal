d5d3e74ce60cdce30b4298fe0ecdbbd10e1344ad
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-10-02 10:24:57 -0300

    adicionei o método toString e import o scanner

--- POO/BancoMaut/Banco/src/App.java
+++ POO/BancoMaut/Banco/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Cliente c1 = new Cliente(nome,CPF,dataNascimento,endereco);
+
+        System.out.println("Seja bem-vindo(a) ao Banco Maut");
     }
 }
--- POO/BancoMaut/Banco/src/Cliente.java
+++ POO/BancoMaut/Banco/src/Cliente.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 public class Cliente {
+
     private String nome;
     private String CPF;
     private String dataNascimento;
-    private String endereco;
-
+    private Endereco endereco;
     
-    public Cliente(String nome, String CPF, String dataNascimento, String endereco) {
+    public Cliente(String nome, String CPF, String dataNascimento, Endereco endereco) {
         this.nome = nome;
         this.CPF = CPF;
         this.dataNascimento = dataNascimento;
@@ -31,14 +33,53 @@ public class Cliente {
         return dataNascimento;
     }
 
-    public String getEndereco() {
+    public Endereco getEndereco() {
         return endereco;
     }
 
-
-    public void setEndereco(String endereco) {
+    public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
 
+    public static Cliente cadastrarCliente() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do cliente:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o CPF do cliente:");
+        String CPF = scanner.nextLine();
+
+        System.out.println("Digite o data de nascimento do cliente:");
+        String dataNascimento = scanner.nextLine();
+
+        System.out.println("Digite a rua do endereço:");
+        String rua = scanner.nextLine();
+
+        System.out.println("Digite o bairro do endereço:");
+        String bairro = scanner.nextLine();
+
+        System.out.println("Digite o cep do endereço:");
+        String cep = scanner.nextLine();
+
+        System.out.println("Digite a cidade do endereço:");
+        String cidade = scanner.nextLine();
+
+        System.out.println("Digite o estado do endereço:");
+        String estado = scanner.nextLine();
+
+        Endereco endereco = new Endereco(rua, bairro, cidade, estado, cep);
+        return new Cliente(nome, dataNascimento, CPF, enderco);
+        
+    }
+
+
+   public String toString(){
+    return "Dados{ " +
+            "Nome: " + nome +
+            "Cpf: " + CPF +
+            "Data de nascimento: " + dataNascimento +
+            "}"; 
+   }
     
 }
